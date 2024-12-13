18ca167ab87132c0df35b0dc736dbf2b0e46ccd0
wasab <wasab@DESKTOP-N7EMA52>
2024-12-14 07:54:56 +0900

    初回

---
+++ HelloWorld_NakajimaYuka/src/sample/SelfIntroduction.java
@@ -0,0 +1,25 @@
+package sample;
+
+public class SelfIntroduction {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+		// 氏名
+		String name = "中嶋　友香";
+		// 年齢
+		int age = 27;
+		// 趣味
+		String hobby = "ドラムを叩くこと";
+
+
+		/*
+		* 自己紹介文を出力する処理
+		*/
+
+		System.out.println("私の名前は" + name + "です。");
+		System.out.println("年齢は" + age + "歳です。");
+		System.out.println("趣味は" + hobby + "です。");
+	}
+
+}
